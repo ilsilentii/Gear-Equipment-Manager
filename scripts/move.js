3c6611bb73e4519e4525d2900ca17ddf4ea4a552
ilsilentii <travis.vdm@outlook.com>
2017-11-24 20:06:42 -0700

    Add files via upload

--- scripts/move.js
+++ scripts/move.js
@@ -1,8 +1,12 @@
 $( document ).ready(function() {
   $(window).mousemove(function(event) {
     $(".move").css({
-      "margin-left": -(event.pageX * 0.03),
-      "margin-top": -(event.pageY * 0.03)
+      "margin-left": -(event.pageX * 0.01),
+      "margin-top": -(event.pageY * 0.01)
+    });
+    $(".moveGear").css({
+      "margin-left": -(event.pageX * 0.02),
+      "margin-top": -(event.pageY * 0.02)
     });
 
 });
